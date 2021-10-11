2553edb7315f38dfd02d02f0aa5f0622161780fd
F5078775 <F5078775@10.203.29.203>
2021-10-11 20:47:47 -0300

    atualizado

--- src/main/java/com/jocimar/bookstore/DevConfig.java
+++ src/main/java/com/jocimar/bookstore/DevConfig.java
@@ -7,6 +7,7 @@ package com.jocimar.bookstore;
 
 import com.jocimar.bookstore.service.DbService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
@@ -20,13 +21,21 @@ import org.springframework.context.annotation.Profile;
 @Configuration
 @Profile("dev")
 public class DevConfig {
+    @Value("${spring.jpa.hibernate.ddl-auto}")
+    private String strategy;
     @Autowired
     private DbService dbService;
     
     
      @Bean
-    public void instaciaBaseDados(){
-        this.dbService.instanciaBaseDados();
+    public boolean instaciaBaseDados(){
+        if(strategy.equals("create")){
+             this.dbService.instanciaBaseDados();
+            
+        }
+        
+        return false;
+       
     }
     
     
